1e864ce006a36810bb7077d32350a70395a0da7d
leehyeonuk <hwlee@beintech.co.kr>
2023-04-13 14:26:56 +0900

    Lamda Method Reference

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,10 @@
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class MainClass {
 
     public static void main(String[] args) {
+        /*
         Plus10 plus10 = new Plus10();
         plus10.apply(10);
 
@@ -10,7 +12,14 @@ public class MainClass {
         Function<Integer,Integer> multi = (i) -> i * 2;
         Function<Integer, Integer> compose = plus.compose(multi);
         System.out.println(compose.apply(2));
+        */
 
-    }
+
+        UnaryOperator<String> test = (s) -> "hi" + s;
+        UnaryOperator<String> hi = Greeting::hi ;
+        System.out.println(test.apply("test"));
+        System.out.println(hi.apply("test"));
+
+        }
     }
 
